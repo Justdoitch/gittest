79b8ada3dd2e2afc1dba67f0cac036bf188daa18
peww99 <peww99@gmail.com>
2023-06-20 19:27:58 +0900

    test3

---
+++ gittest/src/main/java/com/goott/testgit/controller/KimTestController.java
@@ -0,0 +1,18 @@
+package com.goott.testgit.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class KimTestController {
+    
+    @RequestMapping(value="/test/kim", method=RequestMethod.GET)
+    public String requestMethodName(@RequestParam String param) {
+        return "/test/kim";
+    }
+    
+
+}
